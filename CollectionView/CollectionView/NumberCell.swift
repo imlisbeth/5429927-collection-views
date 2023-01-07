f3fec79327a0ee08fcf58f77ce7b8c8c38a6bc35
ليزبيث هالا <llulah@live.com>
2023-01-06 22:18:15 -0300

    set reusable cell

---
+++ CollectionView/CollectionView/NumberCell.swift
@@ -0,0 +1,14 @@
+//
+//  NumberCell.swift
+//  CollectionView
+//
+//  Created by ليزبيث هالا on 06/01/23.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+    static let reuseIdentifier = String(describing: NumberCell.self)
+    
+    @IBOutlet weak var label: UILabel!
+}
