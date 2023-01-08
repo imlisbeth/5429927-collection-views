fe69e17f93ef55e6fcee6f7718c976503cb3b55c
ليزبيث هالا <llulah@live.com>
2023-01-08 20:48:51 -0300

    add and config data source

--- CollectionView/CollectionView/ViewController.swift
+++ CollectionView/CollectionView/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
+    
     @IBOutlet weak var collectionview: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         collectionview.collectionViewLayout = configureLayout()
+        configureDataSource()
     }
 
     func configureLayout() -> UICollectionViewCompositionalLayout {
@@ -26,6 +33,28 @@ class ViewController: UIViewController {
         let section = NSCollectionLayoutSection(group: group)
         
         return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    func configureDataSource() {
+        /*
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) { (collectionView, indexPath, number) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCell.reuseIdentifier, for: indexPath) as? NumberCell else {
+                fatalError("Cannot create new cell")
+            }
+         */
+        dataSource = UICollectionViewDiffableDataSource(collectionView: self.collectionview, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCell.reuseIdentifier, for: indexPath) as? NumberCell else {
+                fatalError("Cannot create new cell")
+            }
+            cell.label.text = itemIdentifier.description
+            return cell
+        })
+        
+        var initialSnapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        initialSnapshot.appendSections([.main])
+        initialSnapshot.appendItems(Array(1...100))
+        
+        dataSource.apply(initialSnapshot, animatingDifferences: false)
     }
 }
 
